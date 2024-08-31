05523068f3448facd98968785f1ae7801b703345
Enes Efe Tokta <enesefetokta009@gmail.com>
2024-08-31 21:48:04 +0300

    Success system update.

--- Fishing/Assets/Success.cs
+++ Fishing/Assets/Success.cs
@@ -4,14 +4,35 @@ using UnityEngine.UI;
 
 public class Success : MonoBehaviour
 {
+    [Header("UI")]
     [SerializeField] private TMP_Text successName;
     [SerializeField] private TMP_Text successDescription;
     [SerializeField] private Image successImage;
 
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+
+    [Header("Object")]
+    [SerializeField] private GameObject successPanel;
+
     public void SuccessShow(SuccessData successData)
     {
+        successPanel.SetActive(true);
+
         successName.text = successData.name;
         successDescription.text = successData.description;
         successImage.sprite = successData.successImage;
+
+        if (animator != null)
+        {
+            animator.SetTrigger("SuccessShow");
+        }
+
+        Invoke("StopSuccessShow", 4.3f);
+    }
+
+    void StopSuccessShow()
+    {
+        successPanel.SetActive(false);
     }
 }
