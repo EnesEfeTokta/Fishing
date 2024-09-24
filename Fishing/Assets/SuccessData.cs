280239adf35eee053d695c79a013c42768618dd9
Enes Efe Tokta <enesefetokta009@gmail.com>
2024-09-24 16:42:37 +0300

    Update SuccessData.cs

--- Fishing/Assets/SuccessData.cs
+++ Fishing/Assets/SuccessData.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 
+// Create a new menu option for creating SuccessData ScriptableObjects in the Unity editor.
 [CreateAssetMenu(fileName = "SuccessData", menuName = "ScriptableObject/SuccessData")]
 public class SuccessData : ScriptableObject
 {
+    // The name of the success (achievement or icon name).
     public new string name;
+
+    // A brief description of the success.
     public string description;
+
+    // A reference to the success image or icon (for UI display).
     public Sprite successImage;
 }
