119430d49250c421591cf91d52b6ae372b121c8d
Enes Efe Tokta <enesefetokta009@gmail.com>
2024-09-24 16:44:29 +0300

    Update Timer.cs

--- Fishing/Assets/Timer.cs
+++ Fishing/Assets/Timer.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    // Reference to the TextMeshPro text that will display the timer value.
     [SerializeField] private TMP_Text timer;
+
+    // Variable to store the elapsed time.
     private float time;
 
     void Update()
     {
+        // Increment the time by the time passed in the current frame.
         time += Time.deltaTime;
+
+        // Convert the time to an integer and update the text display.
         timer.text = Convert.ToInt32(time).ToString();
     }
 }
