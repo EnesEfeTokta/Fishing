0d4b19ec48be7f80892f9c7af2ce4ee2b17fb146
Enes Efe Tokta <enesefetokta009@gmail.com>
2024-09-24 16:43:49 +0300

    Update UICameraLookAt.cs

--- Fishing/Assets/UICameraLookAt.cs
+++ Fishing/Assets/UICameraLookAt.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UICameraLookAt : MonoBehaviour
 {
+    // Reference to the main camera's transform.
     private Transform cameraTransform;
 
     void Start()
     {
+        // Get the main camera's transform on start.
         cameraTransform = Camera.main.transform;
     }
 
     void Update()
     {
+        // Make the object this script is attached to face the camera every frame.
         transform.LookAt(cameraTransform);
     }
 }
