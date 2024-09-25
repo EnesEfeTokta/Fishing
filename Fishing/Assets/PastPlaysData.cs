fd20fa1347143b11d8bee688522a90baecb685ca
Enes Efe Tokta <enesefetokta009@gmail.com>
2024-09-25 15:34:58 +0300

    Create PastPlaysData.cs

---
+++ Fishing/Assets/PastPlaysData.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Create a new menu option for creating PastPlaysData ScriptableObjects in the Unity editor.
+[CreateAssetMenu(fileName = "PastPlaysData", menuName = "ScriptableObject/PastPlaysData")]
+public class PastPlaysData : ScriptableObject
+{
+    // List to store data about past played levels.
+    public List<PastPlayData> pastPlayDatas = new List<PastPlayData>();
+}
+
+[Serializable]
+public class PastPlayData
+{
+    // Icon, name and level number to represent the level.
+    public Sprite icon;
+    public string name;
+    public int levelIndex;
+
+    // Achievements gained from playing the level.
+    public float scoreValue;
+    public float fishValue;
+    public float moneyValue;
+
+    // Maximum possible values for score, fish collected, and money earned in the level.
+    // These represent the highest possible achievements for the given level.
+    public float maxScoreValue;
+    public float maxFishValue;
+    public float maxMoneyValue;
+}
