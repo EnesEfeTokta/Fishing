b3b6fd36fe4d6f7fd6f09dccaf26d2d6ffd77d45
Enes Efe Tokta <enesefetokta009@gmail.com>
2024-10-05 13:17:47 +0300

    Update ShowcaseProductsData.cs and ShopPanel.cs

--- Fishing/Assets/Shop/ShopPanel.cs
+++ Fishing/Assets/Shop/ShopPanel.cs
@@ -2,33 +2,46 @@ using UnityEngine;
 
 public class ShopPanel : MonoBehaviour
 {
+    // Reference to the data container that holds all products for display in the shop.
     [Header("Showcase Products Data")]
     [SerializeField] private ShowcaseProductsData showcaseProductsData;
 
+    // Prefab for individual product display (e.g., a UI element for each product).
     [Header("Product Object Prefab")]
     [SerializeField] private GameObject productPrefab;
 
+    // The parent transform where product UI elements will be instantiated (e.g., a ScrollView content area).
     [Header("Parent")]
     [SerializeField] private Transform parent;
 
+    // Reference to the shop panel game object (which can be opened or closed).
     [Header("Panel")]
     [SerializeField] private GameObject settingsPanel;
+
+    // Boolean flag to track whether the panel is currently open or closed.
     private bool isOpenPanel = false;
 
+    // Method to open or close the shop panel.
     public void PanelOpenClose(bool isOpen)
     {
+        // Enable or disable the settings panel based on the boolean parameter.
         settingsPanel.SetActive(isOpen);
         isOpenPanel = isOpen;
 
+        // Display the products in the shop when the panel is opened.
         ShowProduct();
     }
 
+    // Method to instantiate product UI elements and populate them with product data.
     void ShowProduct()
     {
+        // Loop through all products defined in the showcaseProductsData.
         foreach (ShowcaseProduct showcaseProduct in showcaseProductsData.showcaseProducts)
         {
+            // Instantiate a product prefab as a child of the parent transform.
             ProductCell productCell = Instantiate(productPrefab, parent).GetComponent<ProductCell>();
 
+            // Call the method on the product cell to populate the UI with product information.
             productCell.ShowProductInformation(showcaseProduct);
         }
     }
--- Fishing/Assets/Shop/ShowcaseProductsData.cs
+++ Fishing/Assets/Shop/ShowcaseProductsData.cs
@@ -2,33 +2,53 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script defines a ScriptableObject that holds the list of showcase products for the shop.
+// Each product has its own data such as name, price, image, description, etc.
 [CreateAssetMenu(fileName = "ShowcaseProductsData", menuName = "ScriptableObject/ShowcaseProductsData")]
 public class ShowcaseProductsData : ScriptableObject
 {
+    // List to hold all the products available for showcase in the shop.
     public List<ShowcaseProduct> showcaseProducts = new List<ShowcaseProduct>();
 }
 
 [Serializable]
 public class ShowcaseProduct
 {
+    // Product ID for uniquely identifying each product.
     public int productId = 000;
 
     [Space]
 
+    // Image of the product to be displayed in the UI.
     public Sprite productImage;
+
+    // Name of the product.
     public string productName;
+
+    // Price of the product.
     public int productPrice;
+
+    // Description of the product (TextArea allows multi-line input in the Inspector).
     [TextArea] public string productDescription;
 
     [Space]
 
+    // Boolean flag to indicate whether the product has been purchased.
     public bool isPurchased = false;
+
+    // The date when the product was received after purchase.
     public string productReceivedDate;
 
+    // Method to mark the product as purchased and set the received date to the current date.
     public void StartPurchased()
     {
+        // Get the current date and time.
         DateTime now = DateTime.Now;
+
+        // Convert the date to a string and store it in productReceivedDate.
         productReceivedDate = now.ToString();
+
+        // Mark the product as purchased.
         isPurchased = true;
     }
 }
