84d6298d41e49d88d6a83ce212ddbc9ffc8c32c4
Enes Efe Tokta <enesefetokta009@gmail.com>
2024-10-15 20:43:26 +0300

    Update SettingsFounder.cs

--- Fishing/Assets/Settings/SettingsFounder.cs
+++ Fishing/Assets/Settings/SettingsFounder.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SettingsFounder : MonoBehaviour
@@ -8,6 +10,12 @@ public class SettingsFounder : MonoBehaviour
     [Header("Post-Processing")]
     [SerializeField] private GameObject postProcessingObject; // Reference to the post-processing object to enable or disable based on settings.
 
+    [Header("Music Audio Source")]
+    [SerializeField] private AudioSource musicAudioSource; // Reference to the main music audio source to adjust volume settings.
+
+    [Header("Music Audio Source")]
+    [SerializeField] private List<AudioSource> soundAudioSources = new List<AudioSource>(); // List of sound effect audio sources to adjust their volume levels.
+
     void Start()
     {
         // Initialize post-processing settings based on the player's preferences.
@@ -15,12 +23,40 @@ public class SettingsFounder : MonoBehaviour
 
         // Apply the chosen renderer quality settings.
         ApplyRendererQuality(settingsData.rendererQualityOptions);
+
+        // Adjust the music volume if the music audio source exists.
+        if (musicAudioSource != null)
+        {
+            MusicValueEditing(musicAudioSource, settingsData.musicValue / 100);
+        }
+
+        // Adjust the sound effects volume if the sound audio sources list is not null.
+        if (soundAudioSources != null)
+        {
+            SoundValueEditing(soundAudioSources, settingsData.soundValue / 100);
+        }
     }
 
     // Enables or disables post-processing effects depending on the provided boolean value.
     void PostProcessing(bool isPostProcessing)
     {
         postProcessingObject.SetActive(isPostProcessing); // Activates or deactivates the post-processing GameObject.
+    }
+
+
+    // Adjusts the volume of the main music audio source.
+    void MusicValueEditing(AudioSource audioSource, float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    // Adjusts the volume of all sound effect audio sources.
+    void SoundValueEditing(List<AudioSource> audioSources, float volume)
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            audioSource.volume = volume;
+        }
     }
 
     // Sets the renderer quality level based on the chosen option from the settings.
