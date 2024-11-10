e78a61f006982f546279264f9f0f7eab2bdd43fb
Enes Efe Tokta <enesefetokta009@gmail.com>
2024-11-10 11:42:55 +0300

    Update GameManager.cs

--- Fishing/Assets/GameManager/GameManager.cs
+++ Fishing/Assets/GameManager/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 // Ensures that all necessary components are attached to the GameObject.
@@ -10,18 +11,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
     // Serialized fields to hold references to various data containers.
     [SerializeField] private PastPlaysData pastPlaysData;
     [SerializeField] private PlayerProgressData playerProgressData;
     [SerializeField] private LevelInformationData levelInformationData;
     [SerializeField] private SettingsData settingsData;
 
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     /// <summary>
     /// Provides access to the past plays data.
     /// </summary>
     /// <param name="pastPlaysData">The data object to be populated.</param>
     /// <returns>Returns the populated PastPlaysData object.</returns>
-    public PastPlaysData ReadPastPlaysData(PastPlaysData pastPlaysData)
+    public PastPlaysData ReadPastPlaysData(PastPlaysData pastPlaysData = null)
     {
         return pastPlaysData = this.pastPlaysData;
     }
@@ -31,7 +46,7 @@ public class GameManager : MonoBehaviour
     /// </summary>
     /// <param name="playerProgressData">The data object to be populated.</param>
     /// <returns>Returns the populated PlayerProgressData object.</returns>
-    public PlayerProgressData ReadPlayerProgressData(PlayerProgressData playerProgressData)
+    public PlayerProgressData ReadPlayerProgressData(PlayerProgressData playerProgressData = null)
     {
         return playerProgressData = this.playerProgressData;
     }
@@ -41,7 +56,7 @@ public class GameManager : MonoBehaviour
     /// </summary>
     /// <param name="levelInformationData">The data object to be populated.</param>
     /// <returns>Returns the populated LevelInformationData object.</returns>
-    public LevelInformationData ReadLevelInformationData(LevelInformationData levelInformationData)
+    public LevelInformationData ReadLevelInformationData(LevelInformationData levelInformationData = null)
     {
         return levelInformationData = this.levelInformationData;
     }
@@ -51,8 +66,13 @@ public class GameManager : MonoBehaviour
     /// </summary>
     /// <param name="settingsData">The data object to be populated.</param>
     /// <returns>Returns the populated SettingsData object.</returns>
-    public SettingsData ReadSettingsData(SettingsData settingsData)
+    public SettingsData ReadSettingsData(SettingsData settingsData = null)
     {
         return settingsData = this.settingsData;
+    }
+
+    public (List<GameObject>, List<FishData>) ReadFishCreatAndDeadList()
+    {
+        return (null, null);
     }
 }
