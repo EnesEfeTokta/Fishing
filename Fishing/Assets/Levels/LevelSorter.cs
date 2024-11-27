532a8341422aa2a18e011826c15161d6b94fd997
Enes Efe Tokta <enesefetokta009@gmail.com>
2024-11-27 09:52:15 +0300

    Update LevelSorter.cs

--- Fishing/Assets/Levels/LevelSorter.cs
+++ Fishing/Assets/Levels/LevelSorter.cs
@@ -26,6 +26,25 @@ public class LevelSorter : MonoBehaviour
 
             levelGroupCell.SetLevelGroup(levelGroupData);
             levelGroupCell.CreateLevelCell(levelCellPrefab);
+
+            AdjustGroupHeight(levelGroupCell.transform as RectTransform, levelGroupData.levelInformationDatas.Count);
         }
+    }
+    
+    void AdjustGroupHeight(RectTransform groupTransform, int levelCount)
+    {
+        // Set a height for each level cell (for example, 100F unit height).
+        float levelHeight = 100f;
+
+        // Take into account the gap between the levels (for example, 10F unit space).
+        float spacing = 40f;
+
+        // Calculate the total group height.
+        float totalHeight = (levelCount * levelHeight) + ((levelCount - 1) * spacing);
+
+        // Update the height of Rectransform.
+        Vector2 sizeDelta = groupTransform.sizeDelta;
+        sizeDelta.x = totalHeight;
+        groupTransform.sizeDelta = sizeDelta;
     }
 }
